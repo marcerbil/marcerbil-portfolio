0260d334ec290c7e9c9141473795722e5c73d115
Marc Erbil <marcerbil@gmail.com>
2016-04-21 23:33:31 +0100

    Schmards

--- wp-content/themes/merb/templates/content-home.php
+++ wp-content/themes/merb/templates/content-home.php
@@ -29,7 +29,7 @@
             <div class="about">
                 <div class="row">
                     <div class="custom-shape-container">
-                        <img src="<?php bloginfo('template_directory'); ?>/img/custom-shape.png" class="about-custom-shape" />                        
+                        <img src="<?php bloginfo('template_directory'); ?>/img/custom-shape.png" class="about-custom-shape" />
                     </div>
                     <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
                         <div class="circular-image">
@@ -51,33 +51,84 @@
 
         <hr class="separator-hr" />
     </div>
-
-    <div class="content-container-width-work">
-        <section class="section-projects animated-slower fadeIn">
-
-            <div class="recent-projects-title">
-                <h3 class="recent-projects-title-text">Some of my recent work</h3>
-            </div>
-
-            <div class="row portfolio-row">
-
-                <div class="col-md-6 col-sm-12 col-xs-12">
-                    <div class="portfolio-tile portfolio-post-tile-1">
-                        <a href="../project_one/">
-                            <img src="<?php bloginfo('template_directory'); ?>/img/project-tiles/loft.png" class="portfolio-post-tile-logo" alt="Loft Digital" />
-                        </a>
-                    </div>
-                </div>
-
-                <div class="col-md-6 col-sm-12 col-xs-12">
-                    <div class="portfolio-tile portfolio-post-tile-2">
-                        <a href="../project_two/">
-                            <img src="<?php bloginfo('template_directory'); ?>/img/project-tiles/panacea.png" class="portfolio-post-tile-logo" alt="Panacea Publishing" />
-                        </a>
-                    </div>
-                </div>
-
-            </div>
-        </section>
-    </div>
 </div> <!-- ./content-container -->
+
+<section class="section-recent animated-slower fadeIn">
+
+    <div class="recent-projects-title">
+        <h3 class="recent-projects-title-text">Some of my recent work</h3>
+    </div>
+
+    <div class="row recent-row">
+
+        <div class="col-lg-4">
+            <div class="recent-card-container">
+                <a class="recent-card-link" href="#">
+                    <div class="recent-card recent-card--project">
+                        <div class="recent-card-image-container">
+                            <img class="recent-card-image" src="http://lorempixel.com/320/200" alt="" />
+                        </div>
+                        <div class="recent-card-title">
+                            <h4>weareloft</h4>
+                        </div>
+                        <hr class="recent-card-hr">
+                        <div class="recent-card-text-container">
+                            <p class="recent-card-text">
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                                 Donec odio. Quisque volutpat mattis eros.
+                                  Nullam malesuada erat ut turpis.
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="col-lg-4">
+            <div class="recent-card-container">
+                <a class="recent-card-link" href="#">
+                    <div class="recent-card recent-card--design">
+                        <div class="recent-card-image-container">
+                            <img class="recent-card-image" src="http://lorempixel.com/320/200" alt="" />
+                        </div>
+                        <div class="recent-card-title">
+                            <h4>Daily UI #002</h4>
+                        </div>
+                        <hr class="recent-card-hr">
+                        <div class="recent-card-text-container">
+                            <p class="recent-card-text">
+                                Donec nec justo eget felis facilisis fermentum.
+                                 Aliquam porttitor mauris sit amet orci.
+                                 Aenean dignissim pellentesque felis.
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="col-lg-4">
+            <div class="recent-card-container">
+                <a class="recent-card-link" href="#">
+                    <div class="recent-card recent-card--illustration">
+                        <div class="recent-card-image-container">
+                            <img class="recent-card-image" src="http://lorempixel.com/320/200" alt="" />
+                        </div>
+                        <div class="recent-card-title">
+                            <h4>Manga Character</h4>
+                        </div>
+                        <hr class="recent-card-hr">
+                        <div class="recent-card-text-container">
+                            <p class="recent-card-text">
+                                Morbi in sem quis dui placerat ornare.
+                                Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam.
+                                Sed arcu. Cras consequat.
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+    </div> <!-- /.row .recent-row -->
+</section>
